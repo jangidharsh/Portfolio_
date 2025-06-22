a111591a2706a0b60c18d48918252c65d05630fb
Harsh Jangid <harshjangid14@gmail.com>
2025-06-22 09:51:59 +0530

    frame work complete

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let dropdown = document.getElementsByClassName(".Dropdown")
+let cancelbtn = document.getElementsByClassName("cancel");
+
+// menubtn.addEventListener("click",()=>{
+//     dropdown.style.transform = "translateY(0px)";
+// })
+
+function menubtn(){
+    let menubtn = document.getElementById("#menubtn");
+}
