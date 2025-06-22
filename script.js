bd8d7cf7578cf2ed8d8af158edb03407a4752a4d
Harsh Jangid <harshjangid14@gmail.com>
2025-06-22 10:20:56 +0530

    menu button run

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 
-let dropdown = document.getElementsByClassName(".Dropdown")
-let cancelbtn = document.getElementsByClassName("cancel");
+// let dropdown = document.getElementsByClassName(".Dropdown")
+// let cancelbtn = document.getElementsByClassName("cancel");
 
 // menubtn.addEventListener("click",()=>{
 //     dropdown.style.transform = "translateY(0px)";
 // })
 
-function menubtn(){
-    let menubtn = document.getElementById("#menubtn");
+function hamburg(){
+    const navbar = document.querySelector(".Dropdown");
+    navbar.style.transform ="translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".Dropdown")
+    navbar.style.transform = "translateY(-500px)"
 }
