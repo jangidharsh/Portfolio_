28ebc862bed3a7a447161c29bcd478292d793ab1
Harsh Jangid <harshjangid14@gmail.com>
2025-06-23 10:49:44 +0530

    styel

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-
-// let dropdown = document.getElementsByClassName(".Dropdown")
-// let cancelbtn = document.getElementsByClassName("cancel");
-
-// menubtn.addEventListener("click",()=>{
-//     dropdown.style.transform = "translateY(0px)";
-// })
-
 function hamburg(){
     const navbar = document.querySelector(".Dropdown");
     navbar.style.transform ="translateY(0px)"
@@ -14,3 +6,40 @@ function cancel(){
     const navbar = document.querySelector(".Dropdown")
     navbar.style.transform = "translateY(-500px)"
 }
+
+let git = document.getElementById("github");
+let lkdin = document.getElementById("linkedin");
+let insta = document.getElementById("instagram")
+let fb = document.getElementById("facebook")
+
+git.addEventListener("click", ()=>{
+    window.open("https://github.com/jangidharsh" ,"_blank");
+})
+lkdin.addEventListener("click", ()=>{
+    window.open("https://www.linkedin.com/in/harsh-jangid-7424h/" ,"_blank");
+})
+insta.addEventListener("click", ()=>{
+    window.open("https://www.instagram.com/harshjangid__/?next=%2F" ,"_blank");
+})
+
+let body = document.getElementsByName("body")
+let linkabt = document.getElementById("about-link")
+let abtbtn= document.getElementById("about-btn");
+
+
+let repit =0;
+
+linkabt.addEventListener("click",()=>{
+    if(repit ===0){
+        abtbtn.style.display ="block";
+        abtbtn.style.color ="white"
+        console.log("clicked")
+        repit = 1
+    }
+    else{
+        abtbtn.style.display ="none"
+        console.log("again click")
+        repit = 0
+
+    }
+})
